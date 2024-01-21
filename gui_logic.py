8d71c9c48e441ce43313e96946fbd7dc3258536b
TimofeyVeprev <tima.veprev00@mail.ru>
2024-01-21 18:54:56 +0300

    Init2

--- gui_logic.py
+++ gui_logic.py
@@ -5,9 +5,6 @@ from gui import Ui_Dialog
 class GuiProgram(Ui_Dialog):
 
     def __init__(self, dialog):
-        # ПОЛЯ КЛАССА
-
-        # ДЕЙСТВИЯ ПРИ ВКЛЮЧЕНИИ
         # Создаем окно
         Ui_Dialog.__init__(self)
         self.setupUi(dialog) # Устанавливаем пользовательский интерфейс
